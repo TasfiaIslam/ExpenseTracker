c3644536877df7b3f5cdfda2ce8e4319da105ee5
Tasfia Islam <tasf1a027@gmail.com>
2020-12-25 21:10:42 +0600

    Added custom permission

--- expensetracker/expenses/views.py
+++ expensetracker/expenses/views.py
@@ -7,16 +7,26 @@ from .serializers import ExpenseSerializer
 from .models import Expense
 
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions
 # Create your views here.
 
 
+class ExpenseUserWritePermission(BasePermission):
+    message = 'Editing expenses is restricted to the user only'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
 class ExpenseList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
+    permission_classes = [DjangoModelPermissions]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
 
-class ExpenseDetail(generics.RetrieveAPIView):
+class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView, ExpenseUserWritePermission):
+    permission_classes = [ExpenseUserWritePermission]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer

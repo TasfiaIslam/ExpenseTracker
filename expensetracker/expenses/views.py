8c2f234638d3016835f4ee5b6b4c28121239a697
Tasfia Islam <tasf1a027@gmail.com>
2020-12-25 20:22:20 +0600

    Added view level permission

--- expensetracker/expenses/views.py
+++ expensetracker/expenses/views.py
@@ -7,10 +7,12 @@ from .serializers import ExpenseSerializer
 from .models import Expense
 
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
 
 
 class ExpenseList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser]
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
--- expensetracker/expensetracker/settings.py
+++ expensetracker/expensetracker/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
